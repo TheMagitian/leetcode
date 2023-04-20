f0cc053c1112cae9d742b824353ba64694091084
TheMagitian <127123012+TheMagitian@users.noreply.github.com>
2023-04-20 06:46:00 +0530

    Create M1.c

---
+++ M1.c
@@ -0,0 +1,47 @@
+//Q80
+int removeDuplicates(int* nums, int numsSize) {
+
+    if (nums == NULL || numsSize == 0) return 0;
+
+    int storeIndex = 0;
+
+    int i = 0;
+
+    while (i < numsSize) {
+
+        int count = 1;
+
+        while (i < numsSize - 1 && nums[i] == nums[i + 1]) {
+
+            i++;
+
+            count++;
+
+            if (i >= numsSize) goto OUT; // to prevent returning warnings
+
+        }
+
+        if (count >= 2) {
+
+            nums[storeIndex++] = nums[i];
+
+            nums[storeIndex++] = nums[i];
+
+        }
+
+        else {
+
+            nums[storeIndex++] = nums[i];
+
+        }
+
+        i++;
+
+    }
+
+OUT:
+
+    return storeIndex;
+
+}
+
